18bad55073b8b57da554e5279f1396f064012013
nguyenhongthe <github@nguyenhongthe.dev>
2023-07-25 18:08:48 +0700

    add redirects old url to dot net

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,29 @@ const nextConfig = {
 		appDir: true,
 		mdxRs: true,
 	},
+	async redirects() {
+		return [
+			{
+				source: '/with-basePath', // automatically becomes /docs/with-basePath
+				destination: '/another', // automatically becomes /docs/another
+				permanent: false,
+			},
+			{
+				// does not add /docs since basePath: false is set
+				source: '/knowledge/cai-dat-ssl-cho-xampp-tren-windows',
+				destination: 'https://nguyenhongthe.net/cai-dat-xampp-tren-windows/',
+				basePath: false,
+				permanent: true,
+			},
+			{
+				// does not add /docs since basePath: false is set
+				source: '/knowledge/cai-dat-ssl-cho-xampp-tren-windows',
+				destination: 'https://nguyenhongthe.net/cai-dat-ssl-cho-xampp-tren-windows/',
+				basePath: false,
+				permanent: true,
+			},
+			]
+	},
 };
 
 export default withContentlayer(nextConfig);
