17700a8d239ac61cc18db3f3cfa1b4f02e75a20e
nguyenhongthe <github@nguyenhongthe.dev>
2024-12-22 21:22:36 +0700

    add components Navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,38 @@
+// src/components/Navigation.tsx
+
+import Link from 'next/link'
+import { MenuProps } from '@/src/types/common'
+import { Home } from 'lucide-react'
+import React from 'react'
+
+interface NavigationProps {
+  navigation: MenuProps[];
+}
+
+const Navigation: React.FC<NavigationProps> = ({ navigation }) => {
+  return (
+    <div className='container flex flex-row-reverse items-center justify-between p-6 mx-auto'>
+      <div className='flex justify-between gap-8'>
+        {navigation.map((item) => (
+          <Link
+            key={item.order}
+            href={item.url}
+            title={item.name}
+            className='text-sm duration-500 text-zinc-500 hover:text-zinc-300'
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
+
+      <Link
+        href='/'
+        className='duration-200 text-zinc-300 hover:text-zinc-100'
+      >
+        <Home className='w-6 h-6' />
+      </Link>
+    </div>
+  )
+}
+
+export default Navigation
